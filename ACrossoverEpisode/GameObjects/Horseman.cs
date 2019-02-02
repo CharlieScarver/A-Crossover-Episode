818c3ca329cbf1b68517a960cfbaccdfd308944c
Vlad 'Cryru' Abadzhiev <TheCryru@gmail.com>
2019-02-02 17:25:31 +0200

    Updated to Emotion 269

--- ACrossoverEpisode/GameObjects/Horseman.cs
+++ ACrossoverEpisode/GameObjects/Horseman.cs
@@ -1,17 +1,20 @@
-﻿namespace EmotionPlayground.GameObjects
-{
-    using Emotion.Engine;
-    using Emotion.Game.Animation;
-    using Emotion.Graphics;
-    using Emotion.Graphics.Batching;
-    using Emotion.Graphics.Text;
-    using System.Numerics;
-    using EmotionPlayground.Game.ExtensionClasses;
-    using EmotionPlayground.Interfaces;
-    using System;
-    using System.Collections.Generic;
-    using Emotion.Primitives;
+﻿#region Using
 
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Emotion.Engine;
+using Emotion.Game.Animation;
+using Emotion.Graphics;
+using Emotion.Graphics.Text;
+using Emotion.Primitives;
+using EmotionPlayground.Game.ExtensionClasses;
+using EmotionPlayground.Interfaces;
+
+#endregion
+
+namespace EmotionPlayground.GameObjects
+{
     public class Horseman : Unit, IUpdatable, IDrawable
     {
         // Debug/Visual constants
@@ -41,52 +44,51 @@
 
         public Horseman(Vector3 position, Vector2 size) : base(position, size)
         {
-            this.SpriteSize = new Vector2(48, 48);
-            this.Velocity = 5;
-            this.MaxDashVelocity = 25;
-            this.MaxStamina = 100;
-            this.CurrentStamina = this.MaxStamina;
+            SpriteSize = new Vector2(48, 48);
+            Velocity = 5;
+            MaxDashVelocity = 25;
+            MaxStamina = 100;
+            CurrentStamina = MaxStamina;
 
-            this.maxChageVelocity = 40;
-            this.ChargeVelocity = this.maxChageVelocity;
+            maxChageVelocity = 40;
+            ChargeVelocity = maxChageVelocity;
 
-            this.Animation = new AnimatedTexture(
+            Animation = new AnimatedTexture(
                 Context.AssetLoader.Get<Texture>("horseman-spritesheet.png"),
-                this.SpriteSize,
+                SpriteSize,
                 AnimationLoopType.Normal,
                 900,
                 0,
                 1
             );
 
-            this.BoundingBox = new Rectangle(position.ToVec2(), new Vector2(24, 48));
-            this.BBRelatives = new Vector2(
-                (this.Size.X - this.BoundingBox.Size.X) / 2 + 5,
-                (this.Size.Y - this.BoundingBox.Size.Y) / 2
+            BoundingBox = new Rectangle(position.ToVec2(), new Vector2(24, 48));
+            BBRelatives = new Vector2(
+                (Size.X - BoundingBox.Size.X) / 2 + 5,
+                (Size.Y - BoundingBox.Size.Y) / 2
             );
 
-            this.FeetLevelRelatives = new Vector2();
+            FeetLevelRelatives = new Vector2();
 
-            this.ActionArea = new Circle(new Vector2(position.X + this.Size.X / 2, position.Y + this.Size.Y / 2), ActionAreaRadius);
-            this.Targets = new List<Unit>();
+            ActionArea = new Circle(new Vector2(position.X + Size.X / 2, position.Y + Size.Y / 2), ActionAreaRadius);
+            Targets = new List<Unit>();
 
-            this.TargetCrosshairAnimationTimer = new Timer(500);
-            this.TargetCrosshairPosition = true;
+            TargetCrosshairAnimationTimer = new Timer(500);
+            TargetCrosshairPosition = true;
 
-            this.Timers = new List<Timer>();
-            this.Timers.Add(this.TargetCrosshairAnimationTimer);
-            foreach (Timer timer in this.Timers)
+            Timers = new List<Timer>();
+            Timers.Add(TargetCrosshairAnimationTimer);
+            foreach (Timer timer in Timers)
             {
                 timer.Start();
             }
 
             Context.AssetLoader.Get<Font>("debugFont.otf");
-            buffer = new QuadMapBuffer(4);
         }
 
         private void UpdateTimers(float deltaTime)
         {
-            foreach (Timer timer in this.Timers)
+            foreach (Timer timer in Timers)
             {
                 timer.Update(deltaTime);
             }
@@ -94,59 +96,49 @@
 
         private void ManageInput()
         {
-            this.IsMoving = false;
-            this.IsMovingRight = false;
-            this.IsMovingLeft = false;
+            IsMoving = false;
+            IsMovingRight = false;
+            IsMovingLeft = false;
             //this.IsDashing = false;
 
             if (Context.InputManager.IsKeyDown("D") || Context.InputManager.IsKeyHeld("D"))
             {
-                this.IsFacingRight = true;
-                this.IsMoving = true;
-                this.IsMovingRight = true;
-                this.IsMovingLeft = false;
+                IsFacingRight = true;
+                IsMoving = true;
+                IsMovingRight = true;
+                IsMovingLeft = false;
             }
 
 
             if (Context.InputManager.IsKeyDown("A") || Context.InputManager.IsKeyHeld("A"))
             {
-                this.IsFacingRight = false;
-                this.IsMoving = true;
-                this.IsMovingRight = false;
-                this.IsMovingLeft = true;
+                IsFacingRight = false;
+                IsMoving = true;
+                IsMovingRight = false;
+                IsMovingLeft = true;
             }
 
-            if (Context.InputManager.IsKeyHeld("E") && this.CurrentStamina > 90)
+            if (Context.InputManager.IsKeyHeld("E") && CurrentStamina > 90)
             {
-                this.IsDashing = true;
-                this.DashVelocity = 5;
-                this.DashVelocityIncreaseStep = 1;
+                IsDashing = true;
+                DashVelocity = 5;
+                DashVelocityIncreaseStep = 1;
             }
             else if (Context.InputManager.IsKeyUp("E"))
             {
-                this.IsDashing = false;
+                IsDashing = false;
             }
 
-            if (Context.InputManager.IsKeyDown("Space") && this.CurrentTarget != null)
-            {
-                this.IsCharging = true;
-            }
+            if (Context.InputManager.IsKeyDown("Space") && CurrentTarget != null) IsCharging = true;
 
             if (Context.InputManager.IsKeyUp("Q"))
             {
-                if (this.CurrentTarget != null)
-                {
-                    // Toggle dialog box for Current Target unit
-                    this.CurrentTarget.IsTalking = !this.CurrentTarget.IsTalking;
-                }
+                if (CurrentTarget != null) CurrentTarget.IsTalking = !CurrentTarget.IsTalking;
 
                 // Hide dialog boxes from other units
                 foreach (Unit u in AllUnits)
                 {
-                    if (u != this.CurrentTarget && u.IsTalking)
-                    {
-                        u.IsTalking = false;
-                    }
+                    if (u != CurrentTarget && u.IsTalking) u.IsTalking = false;
                 }
             }
         }
@@ -154,42 +146,33 @@
         // Adds the Charge mechanic to the basic movement handling
         private new void ManageMovement()
         {
-            if (this.IsCharging)
+            if (IsCharging)
             {
-                bool targetIsOnTheRight = this.Position.X < this.CurrentTarget.Position.X;
-                float distanceToTarget = Math.Abs(this.CurrentTarget.X - this.X);
+                bool targetIsOnTheRight = Position.X < CurrentTarget.Position.X;
+                float distanceToTarget = Math.Abs(CurrentTarget.X - X);
                 // When the distance becomes less than one "movement", the next movement equals the distance left
-                if (distanceToTarget < this.ChargeVelocity)
-                {
-                    this.X = this.CurrentTarget.X;
-                }
+                if (distanceToTarget < ChargeVelocity)
+                    X = CurrentTarget.X;
                 else
+                    X += targetIsOnTheRight ? ChargeVelocity : ChargeVelocity * -1;
+                if (Position.X == CurrentTarget.Position.X)
                 {
-                    this.X += (targetIsOnTheRight ? this.ChargeVelocity : (this.ChargeVelocity * -1));
-                }
-                if (this.Position.X == this.CurrentTarget.Position.X)
-                {
-                    this.IsCharging = false;
-                    this.CurrentTarget = null;
+                    IsCharging = false;
+                    CurrentTarget = null;
                 }
             }
-            
-            if (this.Position.X > 5000)
-            {
-                this.IsFalling = true;
-            }
+
+            if (Position.X > 5000) IsFalling = true;
         }
-        
+
         private void ScanForTargets()
         {
             // Start with an empty list to remove Targets who have exited the Action Area
-            this.Targets.Clear();
+            Targets.Clear();
 
             // Remove current target if it's outside of the Action Area
-            if (this.CurrentTarget != null && !Circle.intersectsRectangle(this.ActionArea, this.CurrentTarget.BoundingBox)) //new Rectangle(u.Position.Xy, u.Size)))
-            {
-                this.CurrentTarget = null;
-            }
+            if (CurrentTarget != null && !Circle.intersectsRectangle(ActionArea, CurrentTarget.BoundingBox)) //new Rectangle(u.Position.Xy, u.Size)))
+                CurrentTarget = null;
 
             // I wanted to try this Foreach okay, give a break
             AllUnits.ForEach(o =>
@@ -199,37 +182,32 @@
                 if (ReferenceEquals(u, this)) return;
 
                 // Check if the unit is inside the Action Area
-                if (Circle.intersectsRectangle(this.ActionArea, u.BoundingBox)) //new Rectangle(u.Position.Xy, u.Size)))
+                if (Circle.intersectsRectangle(ActionArea, u.BoundingBox)) //new Rectangle(u.Position.Xy, u.Size)))
                 {
                     // If theres no Current Target, this unit is the new one
-                    if (this.CurrentTarget == null)
+                    if (CurrentTarget == null)
                     {
-                        this.CurrentTarget = u;
+                        CurrentTarget = u;
                     }
                     // If the player's moving right (prioritize movement direction)
-                    else if (this.IsMovingRight)
+                    else if (IsMovingRight)
                     {
                         // And the unit is on their right, this unit is the new Current Target
-                        if (u.X > this.X)
-                        {
-                            this.CurrentTarget = u;
-                        }
+                        if (u.X > X) CurrentTarget = u;
                     }
                     // If the player's moving left (prioritize movement direction)
-                    else if (this.IsMovingLeft)
+                    else if (IsMovingLeft)
                     {
                         // And the unit is on their left, this unit is the new Current Target
-                        if (u.X < this.X)
-                        {
-                            this.CurrentTarget = u;
-                        }
+                        if (u.X < X) CurrentTarget = u;
                     }
                     // If there is a Current Target and the player is not moving right or left
                     // And the unit is closer that the Current Target, that unit is the new Curent Target
-                    else if (Math.Abs(this.X - u.X) < Math.Abs(this.X - this.CurrentTarget.X))
+                    else if (Math.Abs(X - u.X) < Math.Abs(X - CurrentTarget.X))
                     {
-                        this.CurrentTarget = u;
+                        CurrentTarget = u;
                     }
+
                     Targets.Add(u);
                 }
             });
@@ -237,23 +215,18 @@
 
         public override void Update(float deltaTime)
         {
-            this.UpdateTimers(deltaTime);
-            this.ManageInput();
-            this.ManageMovement();
-            
+            UpdateTimers(deltaTime);
+            ManageInput();
+            ManageMovement();
+
             // Don't scan for Targets when charging 
-            if (!this.IsCharging)
-            {
-                this.ScanForTargets();
-            }
+            if (!IsCharging) ScanForTargets();
 
             // Update Action Area position
-            this.ActionArea = new Circle(new Vector2(this.Position.X + this.Size.X / 2, this.Position.Y + this.Size.Y / 2), ActionAreaRadius);
+            ActionArea = new Circle(new Vector2(Position.X + Size.X / 2, Position.Y + Size.Y / 2), ActionAreaRadius);
 
             base.Update(deltaTime);
         }
-
-        MapBuffer buffer;
 
         public override void Draw(Renderer renderer)
         {
@@ -290,40 +263,37 @@
             //renderer.RenderCircleOutline(new Vector3(this.ActionArea.Center), this.ActionArea.Radius, Color.Red, true);
 
             // Draw Stamina bar
-            renderer.Render(this.Position + new Vector3(0, -50, 0), new Vector2(this.CurrentStamina, this.StaminaBarHeight), Color.Green);
-            renderer.RenderLine(this.Position + new Vector3(90, -50, 0), this.Position + new Vector3(90, this.StaminaBarHeight - 50, 0), Color.Yellow);
-            renderer.RenderLine(this.Position + new Vector3(this.MaxStamina, -50, 0), this.Position + new Vector3(this.MaxStamina, this.StaminaBarHeight - 50, 0), Color.Red);
-            renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, "Stamina", this.Position + new Vector3(0, -48, 0), Color.Black);
+            renderer.Render(Position + new Vector3(0, -50, 0), new Vector2(CurrentStamina, StaminaBarHeight), Color.Green);
+            renderer.RenderLine(Position + new Vector3(90, -50, 0), Position + new Vector3(90, StaminaBarHeight - 50, 0), Color.Yellow);
+            renderer.RenderLine(Position + new Vector3(MaxStamina, -50, 0), Position + new Vector3(MaxStamina, StaminaBarHeight - 50, 0), Color.Red);
+            renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, "Stamina", Position + new Vector3(0, -48, 0), Color.Black);
             // Draw name
-            renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, "Michael Horseman", new Vector3(this.X, this.Y - 20, this.Z), Color.Black);
+            renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, "Michael Horseman", new Vector3(X, Y - 20, Z), Color.Black);
 
-            if (this.CurrentTarget != null)
-            {
-                // Draw current target ID
-                renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, this.CurrentTarget.Id.ToString(), this.Position + new Vector3(-20, 0, 0), Color.Red);
-            }
+            if (CurrentTarget != null) renderer.RenderString(Context.AssetLoader.Get<Font>("debugFont.otf"), 15, CurrentTarget.Id.ToString(), Position + new Vector3(-20, 0, 0), Color.Red);
 
             // Draw crosshairs on Targets
-            foreach (Unit target in this.Targets)
+            foreach (Unit target in Targets)
             {
-                bool isCurrentTarget = target == this.CurrentTarget;
+                bool isCurrentTarget = target == CurrentTarget;
                 Color crosshairColor = isCurrentTarget ? Color.Red : Color.Black;
 
-                if (this.TargetCrosshairAnimationTimer.Ready)
+                if (TargetCrosshairAnimationTimer.Ready)
                 {
-                    this.TargetCrosshairAnimationTimer.Start();
-                    this.TargetCrosshairPosition = !this.TargetCrosshairPosition;
+                    TargetCrosshairAnimationTimer.Start();
+                    TargetCrosshairPosition = !TargetCrosshairPosition;
                 }
 
                 // Crosshair "animation"
-                if (this.TargetCrosshairPosition)
+                if (TargetCrosshairPosition)
                 {
                     Circle c = new Circle(target.Center, target.Size.X / 1.5f);
                     renderer.RenderLine(new Vector3(c.Left, target.Z), new Vector3(c.Top, target.Z), crosshairColor);
                     renderer.RenderLine(new Vector3(c.Top, target.Z), new Vector3(c.Right, target.Z), crosshairColor);
                     renderer.RenderLine(new Vector3(c.Right, target.Z), new Vector3(c.Bottom, target.Z), crosshairColor);
                     renderer.RenderLine(new Vector3(c.Bottom, target.Z), new Vector3(c.Left, target.Z), crosshairColor);
-                } else
+                }
+                else
                 {
                     Rectangle r = new Rectangle(target.Position.ToVec2(), new Vector2(target.Size.X, target.Size.X));
                     renderer.RenderLine(new Vector3(r.X, r.Y, target.Z), new Vector3(r.X + r.Width, r.Y, target.Z), crosshairColor);
